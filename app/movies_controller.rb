960bdba2e2ac82252d75d8bca9f8d9b0f350c446
Angeleen MCDONNELL Wilson <angeleenmc@gmail.com>
2020-04-02 06:19:44 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -68,7 +68,6 @@ def can_be_found_updated_and_saved
   # Updtate the title "Awesome Flick" to "Even Awesomer Flick", save it, then return it
   Movie.create(title: "Awesome Flick")
   Movie.where(title: "Awesome Flick").update(title: "Even Awesomer Flick")
-  movie
 end
 
 def can_update_using_update_method
